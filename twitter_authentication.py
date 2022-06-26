eac546c36fcf8a080d0b0ff29a88fdcbe9d01d77
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2022-06-25 18:13:52 -0700

    Update twitter_authentication.py

--- twitter_authentication.py
+++ twitter_authentication.py
@@ -1,8 +1,3 @@
-# bearer_token = 'AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejrcLFVkq6D%2F%2F1ZQ8RzxH6Hb8Tw%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
-
-# Prof. Rahaman's bearer token
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAPdbdgEAAAAAKZgErYDAxP5U8BnHsROiQ12R6%2Fg%3DIJWGuYFeRYjMEWmEH4E4njwG5yV3vygtSgdtw0Q2AWuBxyiiuS'
+# bearer_token = ''
 
 
-# Prof. Hasan's bearer token
-# bearer_token = "AAAAAAAAAAAAAAAAAAAAAOTeVAEAAAAAWreq4uJHqSwTTdEelgTGTXlB%2BR0%3DPM5VfIc4koBQ1OOgavSWEU4csuVZ1OO1yWmncB5E0rIP9BID3b"
