a8b6480a26f57c741fefdb0ab771622aee1e76d2
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2022-06-25 19:14:41 -0700

    Update collect_tweet_keywords.py

--- collect_tweet_keywords.py
+++ collect_tweet_keywords.py
@@ -31,28 +31,3 @@ print(tag_dict)
 
 
 
-'''
-{
-    'urls': [{
-        'start': 47, 
-        'end': 70, 
-        'url': 'https://t.co/IFDpnq1cwh', 
-        'expanded_url': 'https://youtube.com/channel/UCklyxTTBB0WuTmHRcWbw9IQ/live', 
-        'display_url': 'youtube.com/channel/UCklyx‚Ä¶', 
-        'status': 200, 
-        'title': 'üî¥ JOGANDO COM OS INSCRITOS | ROBLOX AO VIVO | ROBUX - YouTube', 
-        'description': 'üî∏Para ver a descri√ß√£o, clique em: MOSTRAR MAIS.Participe do sorteio de robux, lendo a descri√ß√£o para saber mais. E ganhar robux gr√°tis em nossa liveVisite o...', 
-        'unwound_url': 'https://www.youtube.com/channel/UCklyxTTBB0WuTmHRcWbw9IQ/live'}], 
-    'mentions': [{
-        'start': 79, 
-        'end': 86, 
-        'username': 'Roblox', 
-        'id': '16745055'
-        }], 
-    'hashtags': [{
-        'start': 87, 
-        'end': 94, 
-        'tag': 'Roblox'
-        }]
-    }
-'''
