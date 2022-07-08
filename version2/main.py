39c9211f500372bf9e696eb150f3d44dda16112a
Xin Li <xinli2@email.arizona.edu>
2022-07-08 16:17:58 -0700

    update

--- version2/main.py
+++ version2/main.py
@@ -9,7 +9,7 @@ import combine_dataframes
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("--task", type=str, required=True, 
-        choices=["collect-tweet", "clean-csv-dfs", "merge-dfs", "collect-reddit"], 
+        choices=["collect-tweet", "clean-csv-dfs", "merge-dfs", "collect-reddit","collect-stackoverflow"], 
         help='what do you want to do?')
     parser.add_argument("--out_dir", required=True, type=str, 
         default='', help='out put directory')
@@ -26,6 +26,23 @@ def parse_args() -> argparse.Namespace:
 
 
 if __name__ == "__main__":
+    print("Hello, Welcome to SMSA Toolkit. What can I do for you?")
+    menu = """
+                                                                
+      _______   __     __       ____        _______    __________   
+     / ______| |  \\  /  |      /    \\      / _____|   /  /   /  /|
+     |  \___   |   \\/   |     /  /\  \\     | \___    /_ /___/ _/ | 
+      \____ \\  |        |    /  /__\\  \\     \____ \\  |         | |
+      _____| | | |\\  /| |   /  ______  \\   _____| |  |         | |
+     |______/  |_| \\/ |_|  /__/      \\__\\  |______/  |_________|/
+                                 
+                                              
+        """
+    print(menu)
+    print('=========================================================================')
+    print('                        Welcome to SMSA Toolkit')
+    print('=========================================================================')
+    print('                 Use -h to list possible arguments')
     args = parse_args()
 
     if  args.task=='merge-dfs':
