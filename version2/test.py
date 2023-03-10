7cbf5d4d47800850f1da6c854a1e905f7d2f4109
Li Xin (Jack) <77429846+xinli2@users.noreply.github.com>
2023-03-10 00:17:59 -0700

    Update test.py

--- version2/test.py
+++ version2/test.py
@@ -11,3 +11,14 @@ menu = """
                                               
         """
 print(menu)
+
+Ld=MethodLoader(builtFilePath, direct, file)
+                    self.attackMethod_dict = Ld.set_attackMethod_dict()
+                    for md in self.attackMethod:
+                        self.methodname = md
+                        if self.methodname in self.attackMethod_dict.keys():
+                            Ld.add_attackMethodName(self.methodname)
+                            task = Ld.run()
+                            task()
+                        else:
+                            pass
